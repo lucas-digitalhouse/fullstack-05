073feeb275373366058c31ee3b38913446b01d28
Lucas Chiarizia <lucaschiarizia@PDMC02D2DSMP3XY.local>
2021-06-16 22:54:22 -0300

    15 de junio

--- modulo2/cierreModulo/concecionaria.js
+++ modulo2/cierreModulo/concecionaria.js
@@ -3,15 +3,20 @@ let autos = require ("./autos.js")
 const concesionaria = {
     // es posible crear la funcionalidad buscarAuto que reciba por parámetro 
     //la patente y devuelva el auto al cual le corresponde. En caso de no encontrar el mismo, deberá retornar null.
-   buscarAutos: function(patente){
+   buscarAuto: function(patente){
        let autos = this.autos
-       let autoBuscado = autos.filter(function(todosLosAutos){
+       let autoBuscado = autos.find(function(todosLosAutos){
         return todosLosAutos.patente == patente ;
-       }), 
+       })
+       if (autoBuscado != undefined){
        return autoBuscado
+       } else { return null
+
+       }
+
     },
    venderAutos: function (){},
    autos: autos,
 };
 
-cosole.log(consecionaria.buscarAutos(JJK116))
+console.log(concesionaria.buscarAuto("JJK116"))
